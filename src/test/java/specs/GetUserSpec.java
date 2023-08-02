289ea1b4bca10a7f3d50ee6a117c9a50895e1d09
Evgenii Klimashin <evgeniiklimashin99@gmail.com>
2023-08-02 19:43:32 +0300

    Added custom allure report

--- src/test/java/specs/GetUserSpec.java
+++ src/test/java/specs/GetUserSpec.java
@@ -4,10 +4,10 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
-import static io.restassured.http.ContentType.JSON;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class GetUserSpec {
@@ -15,7 +15,7 @@ public class GetUserSpec {
             .log().uri()
             .log().method()
             .log().body()
-            .contentType(JSON);
+            .filter(withCustomTemplates());
     public static ResponseSpecification getUserResponseSpecStatusCodeIs200 = new ResponseSpecBuilder()
             .log(STATUS)
             .log(BODY)
